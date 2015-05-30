a588d5efa47853a176bcc600b0380a5569cf31d7
gnuchoi <gnuchoi@gmail.com>
2015-05-30 14:46:53 +0100

    update setup environment file

--- setup_environment.py
+++ setup_environment.py
@@ -1,12 +1,29 @@
 import platform
 deviceName = platform.node()
 
-PHD_PATH = '/Users/gnu/Gnubox/Study_PhD/'
 
-SRC_PATH = '/Users/gnu/Gnubox/Srcs/music/'
+if deviceName == 'KChoi.MBPR.2013.home':
+	isLaptop = True
+	isServer = False
+	isExeter = False
+elif deviceName == 'eceter.eecs.qmul.ac.uk':
+	isLaptop = False
+	isServer = True
+	isExeter = True
+
+
+if isLaptop:
+	PHD_PATH = '/Users/gnu/Gnubox/Study_PhD/'
+
+	SRC_PATH = '/Users/gnu/Gnubox/Srcs/music/'
+else:
+	PHD_PATH = '~/'
+	SRC_PATH = '~/srcs/'
+
 
 
 PROJ_FOLDER = 'nvr/'
 DATA_FOLDER = 'data/'
 
+PROJ_PATH = PHD_PATH + PROJ_FOLDER
 DATA_PATH = PHD_PATH + PROJ_FOLDER + DATA_FOLDER
